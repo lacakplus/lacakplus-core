728d06f35bf0be60bfa6e8a61d3253aa09fdc221
Samsud Dhuha <samsud.dhuha@mamiteam.com>
2023-06-13 22:25:47 +0700

    update router api

--- app/routers/router.js
+++ app/routers/router.js
@@ -3,6 +3,7 @@ let router = express.Router();
 let auth = require("../middleware/auth.js");
 // const upload = require("../middleware/upload.js");
  
+//  userController
 const user = require('../controllers/userController.js');
 // router.get('/api/user/all', auth.verifyToken, user.getUsers);
 router.post('/api/user/detail', auth.verifyToken, user.getUserById);
@@ -12,10 +13,16 @@ router.post('/api/user/detail', auth.verifyToken, user.getUserById);
 router.post('/api/login', user.login);
 router.post('/api/register', user.register);
 
-const version = require('../controllers/versionController.js');
-router.post('/api/version/apps', version.getVersion);
+// const version = require('../controllers/versionController.js');
+// router.post('/api/version/apps', version.getVersion);
 
-// const uploadController = require("../controllers/uploadController.js");
-// router.post("/api/upload", auth.verifyToken, upload.single("file"), uploadController.upload);
+//  roleController
+const role = require("../controllers/roleController.js");
+router.post('/api/role', auth.verifyToken, role.getRoles);
+
+//  vehicleController
+const vehicle = require("../controllers/vehicleController.js");
+router.post('/api/vehicle', auth.verifyToken, vehicle.getVehicles);
+router.post('/api/vehicle/add', auth.verifyToken, vehicle.addVehicle);
 
 module.exports = router;
