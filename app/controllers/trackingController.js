ac51bf43173fc3c6dbee140fd96a9c3c7304849f
wahyu.aditya523 <Wahyu.aditya523@gmail.com>
2023-08-24 21:34:32 +0700

    Update trackingController.js
    Fix query getVehiclePos

--- app/controllers/trackingController.js
+++ app/controllers/trackingController.js
@@ -50,7 +50,8 @@ exports.getVehiclePos = (request, response) => {
 
     const query = 'SELECT * FROM ( '+
         'SELECT v.name, t.lat, t.lng, t.created_at, ROW_NUMBER() OVER (ORDER BY t.created_at DESC) as RN FROM tr_tracking t '+
-        'JOIN m_vehicle v ON v.id = t.id_vehicle AND v.id_company = ? '+
+        'JOIN tr_travel tv ON tv.id = t.id_travel AND tv.id_company = ? '+
+        'JOIN m_vehicle v ON v.id = tv.id_vehicle '+
         ') sub WHERE RN = 1'
 
     db.pool.query(query, [id_company], (error, results) => {
