553baa10371e3a2b79900c77988efc835eced8fe
Samsud Dhuha <samsud.dhuha@mamiteam.com>
2023-12-07 00:17:22 +0700

    add attendance controller

--- app/controllers/attendanceController.js
+++ app/controllers/attendanceController.js
@@ -2,7 +2,7 @@ const db = require('../config/dbConfig.js');
 const statusCode = require('../config/statusCode.js');
 
 exports.addAtendance = (request, response) => {
-    const user_id = request.user_id
+    const userId = request.userId
     const id_company = request.body.id_company
     const attendance_at = request.body.attendance_at
     const status = request.body.status
@@ -10,7 +10,7 @@ exports.addAtendance = (request, response) => {
     const location = request.body.location
 
     const queryString = 'INSERT INTO tr_attendance (id_company, attendance_at, status, photo, location, creator_id) VALUES (?, ?, ?, ?, ?, ?)'
-    db.pool.query(queryString, [id_company, attendance_at, status, photo, location, user_id], (error, results) => {
+    db.pool.query(queryString, [id_company, attendance_at, status, photo, location, userId], (error, results) => {
         if (error) {
             return response.status(statusCode.bad_request).send({
                 code: statusCode.bad_request,
--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -20,7 +20,6 @@ exports.verifyToken = (request, res, next) => {
         });
       }
       request.userId = decoded.id;
-      request.user_id = decoded.id;
       next();
     });
 };
