07aaad6669121ef5d3237f47bf7f53df9ebd374f
Samsud Dhuha <samsud.dhuha@mamiteam.com>
2023-12-07 00:31:56 +0700

    test

--- app/controllers/attendanceController.js
+++ app/controllers/attendanceController.js
@@ -1,7 +1,7 @@
-import { pool } from '../config/dbConfig.js';
-import { bad_request, success } from '../config/statusCode.js';
+const db = require('../config/dbConfig.js');
+const statusCode = require('../config/statusCode.js');
 
-export function addAtendance(request, response) {
+exports.addAtendance = (request, response) => {
     const user_id = request.user_id
     const id_company = request.body.id_company
     const attendance_at = request.body.attendance_at
@@ -10,18 +10,18 @@ export function addAtendance(request, response) {
     const location = request.body.location
 
     const queryString = 'INSERT INTO tr_attendance (id_company, attendance_at, status, photo, location, creator_id) VALUES (?, ?, ?, ?, ?, ?)'
-    pool.query(queryString, [id_company, attendance_at, status, photo, location, user_id], (error, results) => {
+    db.pool.query(queryString, [id_company, attendance_at, status, photo, location, userId], (error, results) => {
         if (error) {
-            return response.status(bad_request).send({
-                code: bad_request,
+            return response.status(statusCode.bad_request).send({
+                code: statusCode.bad_request,
                 message: error.message,
                 error: error
             });
         }
 
         let message = (status == 0) ? "Berhasil Clock In" : "Berhasil Clock Out"
-        response.status(success).send({
-            code: success,
+        response.status(statusCode.success).send({
+            code: statusCode.success,
             message: message
         });
     })
--- app/routers/router.js
+++ app/routers/router.js
@@ -1,78 +1,78 @@
-import { Router } from 'express';
-let router = Router();
-import { verifyToken } from "../middleware/auth.js";
-import { single } from "../middleware/upload.js";
+let express = require('express');
+let router = express.Router();
+let auth = require("../middleware/auth.js");
+let upload = require("../middleware/upload.js");
 
 //  uploadController
-import { upload as _upload } from "../controllers/uploadController.js";
-router.post("/api/upload", verifyToken, single("file"), _upload);
+const uploadController = require("../controllers/uploadController.js");
+router.post("/api/upload", auth.verifyToken, upload.single("file"), uploadController.upload);
 
  
 //  userController
-import { getUsers, getUserById, addUser, editUser, deleteUser, login, register } from '../controllers/userController.js';
-router.post('/api/users', verifyToken, getUsers);
-router.post('/api/user', verifyToken, getUserById);
-router.post('/api/user/add', verifyToken, addUser);
-router.post('/api/user/edit', verifyToken, editUser);
-router.post('/api/user/delete', verifyToken, deleteUser);
+const user = require('../controllers/userController.js');
+router.post('/api/users', auth.verifyToken, user.getUsers);
+router.post('/api/user', auth.verifyToken, user.getUserById);
+router.post('/api/user/add', auth.verifyToken, user.addUser);
+router.post('/api/user/edit', auth.verifyToken, user.editUser);
+router.post('/api/user/delete', auth.verifyToken, user.deleteUser);
 
-router.post('/api/login', login);
-router.post('/api/register', register);
+router.post('/api/login', user.login);
+router.post('/api/register', user.register);
 
 
 //  attendanceController
-import { addAtendance } from '../controllers/attendanceController.js';
-router.post('/api/attendance/add', verifyToken, addAtendance);
+const attendance = require('../controllers/attendanceController.js');
+router.post('/api/attendance/add', auth.verifyToken, attendance.addAtendance);
 
 // const version = require('../controllers/versionController.js');
 // router.post('/api/version/apps', version.getVersion);
 
 //  roleController
-import { getRoles } from "../controllers/roleController.js";
-router.get('/api/roles', verifyToken, getRoles);
+const role = require("../controllers/roleController.js");
+router.get('/api/roles', auth.verifyToken, role.getRoles);
 
 //  vehicleController
-import { getVehicles, getVehicleById, addVehicle, editVehicle, deleteVehicle } from "../controllers/vehicleController.js";
-router.post('/api/vehicles', verifyToken, getVehicles);
-router.post('/api/vehicle', verifyToken, getVehicleById);
-router.post('/api/vehicle/add', verifyToken, addVehicle);
-router.post('/api/vehicle/edit', verifyToken, editVehicle);
-router.post('/api/vehicle/delete', verifyToken, deleteVehicle);
+const vehicle = require("../controllers/vehicleController.js");
+router.post('/api/vehicles', auth.verifyToken, vehicle.getVehicles);
+router.post('/api/vehicle', auth.verifyToken, vehicle.getVehicleById);
+router.post('/api/vehicle/add', auth.verifyToken, vehicle.addVehicle);
+router.post('/api/vehicle/edit', auth.verifyToken, vehicle.editVehicle);
+router.post('/api/vehicle/delete', auth.verifyToken, vehicle.deleteVehicle);
 
 //  companyController
-import { getCompanies, getCompanyById, editCompany, deleteCompany } from "../controllers/companyController.js";
-router.get('/api/companies', verifyToken, getCompanies);
-router.post('/api/company', verifyToken, getCompanyById);
-router.post('/api/company/edit', verifyToken, editCompany);
-router.post('/api/company/delete', verifyToken, deleteCompany);
+const company = require("../controllers/companyController.js");
+router.get('/api/companies', auth.verifyToken, company.getCompanies);
+router.post('/api/company', auth.verifyToken, company.getCompanyById);
+router.post('/api/company/edit', auth.verifyToken, company.editCompany);
+router.post('/api/company/delete', auth.verifyToken, company.deleteCompany);
 
 //  locationController
-import { getLocations, getLocationById, addLocation, editLocation, deleteLocation } from "../controllers/locationController.js";
-router.post('/api/locations', verifyToken, getLocations);
-router.post('/api/location', verifyToken, getLocationById);
-router.post('/api/location/add', verifyToken, addLocation);
-router.post('/api/location/edit', verifyToken, editLocation);
-router.post('/api/location/delete', verifyToken, deleteLocation);
+const location = require("../controllers/locationController.js");
+router.post('/api/locations', auth.verifyToken, location.getLocations);
+router.post('/api/location', auth.verifyToken, location.getLocationById);
+router.post('/api/location/add', auth.verifyToken, location.addLocation);
+router.post('/api/location/edit', auth.verifyToken, location.editLocation);
+router.post('/api/location/delete', auth.verifyToken, location.deleteLocation);
 
 //  travelController
-import { addTravel, getTravels, getTravelById, editTravel, activeTravel, travelStart, travelArriveCustomer, travelDepartCustomer, travelComplete, getTravelTotal, getTravelDetails, editTravelDetails } from "../controllers/travelController.js";
-router.post('/api/travel/add', verifyToken, addTravel);
-router.post('/api/travels', verifyToken, getTravels);
-router.post('/api/travel', verifyToken, getTravelById);
-router.post('/api/travel/edit', verifyToken, editTravel);
-router.post('/api/travel/active-travel', verifyToken, activeTravel);
-router.post('/api/travel/start', verifyToken, travelStart);
-router.post('/api/travel/arrive-customer', verifyToken, travelArriveCustomer);
-router.post('/api/travel/depart-customer', verifyToken, travelDepartCustomer);
-router.post('/api/travel/complete', verifyToken, travelComplete);
-router.post('/api/travel/total', verifyToken, getTravelTotal);
-router.post('/api/travel-details', verifyToken, getTravelDetails);
-router.post('/api/travel-details/edit', verifyToken, editTravelDetails);
+const travel = require("../controllers/travelController.js");
+router.post('/api/travel/add', auth.verifyToken, travel.addTravel);
+router.post('/api/travels', auth.verifyToken, travel.getTravels);
+router.post('/api/travel', auth.verifyToken, travel.getTravelById);
+router.post('/api/travel/edit', auth.verifyToken, travel.editTravel);
+router.post('/api/travel/active-travel', auth.verifyToken, travel.activeTravel);
+router.post('/api/travel/start', auth.verifyToken, travel.travelStart);
+router.post('/api/travel/arrive-customer', auth.verifyToken, travel.travelArriveCustomer);
+router.post('/api/travel/depart-customer', auth.verifyToken, travel.travelDepartCustomer);
+router.post('/api/travel/complete', auth.verifyToken, travel.travelComplete);
+router.post('/api/travel/total', auth.verifyToken, travel.getTravelTotal);
+router.post('/api/travel-details', auth.verifyToken, travel.getTravelDetails);
+router.post('/api/travel-details/edit', auth.verifyToken, travel.editTravelDetails);
 
 //  trackingController
-import { getTrackings, addTracking, getPositionVehicles } from "../controllers/trackingController.js";
-router.post('/api/trackings', verifyToken, getTrackings);
-router.post('/api/tracking/add', verifyToken, addTracking);
-router.post('/api/tracking/position-vehicles', verifyToken, getPositionVehicles);
+const tracking = require("../controllers/trackingController.js");
+router.post('/api/trackings', auth.verifyToken, tracking.getTrackings);
+router.post('/api/tracking/add', auth.verifyToken, tracking.addTracking);
+router.post('/api/tracking/position-vehicles', auth.verifyToken, tracking.getPositionVehicles);
 
-export default router;
+module.exports = router;
