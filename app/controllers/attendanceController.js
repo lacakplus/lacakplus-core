08db082fe895e3feb1afa32240094827f0a49440
Samsud Dhuha <samsud.dhuha@mamiteam.com>
2023-12-10 12:42:56 +0700

    get attendance today

--- app/controllers/attendanceController.js
+++ app/controllers/attendanceController.js
@@ -21,3 +21,25 @@ exports.addAtendance = (request, response) => {
         });
     })
 }
+
+exports.getAttendanceToday = (request, response) => {
+    const user_id = request.user_id
+
+    let date_ob = new Date();
+    let date = ("0" + date_ob.getDate()).slice(-2);
+    let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+    let year = date_ob.getFullYear();
+    
+    let dateNow = year + "-" + month + "-" + date
+    
+    let queryString = "SELECT * FROM tr_attendance WHERE id = ? AND flag = 1 AND attendance_at = ?"
+    db.pool.query(queryString, [user_id, dateNow], (error, results) => {
+        baseError.handleError(error, response)
+
+        response.status(statusCode.success).send({
+            code: statusCode.success,
+            message: message,
+            data: results
+        });
+    })
+}
