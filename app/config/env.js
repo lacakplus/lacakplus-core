db7f9df356c6a7de89bfdea8c9c263a7aa1bbd04
Samsud Dhuha <samsud.dhuha@mamiteam.com>
2023-12-29 08:38:24 +0700

    update password

--- app/config/env.js
+++ app/config/env.js
@@ -1,7 +1,7 @@
 const env = {
   database: 'bbr06y6fjhbgszqqjxtn',
   username: 'uylkpxqmwg3zrmbo',
-  password: 'Porjaw-giwpun-9bidqi',
+  password: 'h2y5PAGCYS2S5fyIse0Z',
   host: 'bbr06y6fjhbgszqqjxtn-mysql.services.clever-cloud.com',
 };
 
