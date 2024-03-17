40af1d5e1e96a61d524ce347c8a209bf5ccfb257
Samsud Dhuha <samsud.dhuha@mamiteam.com>
2024-03-17 20:14:28 +0700

    add tmp folder

--- app/middleware/upload.js
+++ app/middleware/upload.js
@@ -8,11 +8,14 @@ const imageFilter = (req, file, cb) => {
 var storage = multer.diskStorage({
   destination: (req, file, cb) => {
     if (file.mimetype.startsWith("image")) {
-      cb(null, "./resource/image");
+      // cb(null, "./resource/image");
+      cb(null, "/tmp");
     } else if (file.mimetype.startsWith("video")) {
-      cb(null, "./resource/video");
+      // cb(null, "./resource/video");
+      cb(null, "/tmp");
     } else {
-      cb(null, "./resource/file");
+      // cb(null, "./resource/file");
+      cb(null, "/tmp");
     }
   },
   filename: (req, file, cb) => {
